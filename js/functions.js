550b57ec094e78721ee6d76e8f6fc2a8c2b8d8f1
Mathieu Le Roi <code@mathieuleroi.com>
2016-03-23 22:05:06 +0100

    mmenu setup in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,27 @@
-define(['jquery', 'core/theme-app', 'theme/js/bootstrap.min'],function($, App){
+define(['jquery', 'core/theme-app', 'theme/js/jquery.mmenu.all.min'],function($, App){
     
-	//See http://uncategorized-creations.com/app-kit/doc/#237-functions-js to 
-	//learn more about what you can do in this functions.js file.
+	App.on( 'info:app-ready', function(){
+		
+		$( "#menu" ).mmenu( 
+			{
+				extensions: [ "theme-black" ],
+				navbar: false,
+				navbars: {
+					height: 4,
+					content: [
+						'<a href="4058426638" class="fa fa-phone"></a>',
+						'<img src="img/bmchs.png" />',
+						'<a href="https://mail.google.com/mail/u/0/#inbox " class="fa fa-envelope"></a>'
+					]
+				}
+			}
+		);
+
+		$("#menu").on("click","a",function(){
+			App.navigate($(this).attr('href'));
+		});
 	
+	});
 	
 	/**
 	 * Scroll to top each time a new screen is showed in the app.
@@ -13,7 +32,6 @@ define(['jquery', 'core/theme-app', 'theme/js/bootstrap.min'],function($, App){
 		scrollTop();
 	} );
 	
-	
 	/**
 	 * Get back to the top of the screen
 	 */
@@ -21,25 +39,4 @@ define(['jquery', 'core/theme-app', 'theme/js/bootstrap.min'],function($, App){
 		window.scrollTo( 0, 0 );
 	}
 	
-});
-
-define(['js/jquery.mmenu.all.min.js'],function(){
-	$(function() {
-				$("#menu")
-					.mmenu({
-		extensions 	: [ "theme-black" ],
-		navbar 		: false,
-		navbars		: {
-			height 	: 4,
-			content : [ 
-				'<a href="4058426638" class="fa fa-phone"></a>',
-				'<img src="img/bmchs.png" />',
-				'<a href="https://mail.google.com/mail/u/0/#inbox " class="fa fa-envelope"></a>'
-			]
-		}
-					});
-});
-
-define(['js/jquery-1.8.3.min.js'],function(){
-     //Nothing for now, this is just to include the js file in our theme.
 });
